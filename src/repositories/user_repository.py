6c014bdb6ca960f16f3963e73e905f71d437112f
annica-henriette <annica.helsingius@gmail.com>
2024-04-06 11:03:10 +0300

    corrected spelling

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -2,7 +2,7 @@ from entities.user import User
 from database_connection import get_database_connection
 
 def get_user_by_row(row):
-    return User(row["username"], row["parrword"]) if row else None
+    return User(row["username"], row["password"]) if row else None
 
 class UserRepository:
 
@@ -12,9 +12,7 @@ class UserRepository:
     def find_all(self):
 
         cursor = self._connection.cursor()
-
         cursor.execute("select * from users")
-
         rows = cursor.fetchall()
 
         return list(map(get_user_by_row, rows))
@@ -22,12 +20,10 @@ class UserRepository:
     def create(self, user):
 
         cursor = self._connection.cursor()
-
         cursor.execute(
             "insert into users (username, password) values (?, ?)",
             (user.username, user.password)
         )
-
         self._connection.commit()
 
         return user
