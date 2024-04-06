82832e0701ee27927c7dab5774a386b585f1b9e7
annica-henriette <annica.helsingius@gmail.com>
2024-04-06 11:52:08 +0300

    added workout

---
+++ src/entities/workout.py
@@ -0,0 +1,9 @@
+# import uuid
+
+class Workout:
+
+    def __init__(self, content, user=None, workout_id=None):
+        
+        self.content = content
+        self.user = user
+        self.id = workout_id
