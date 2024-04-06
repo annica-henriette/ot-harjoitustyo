4e91a7f95914a51190b04e9cb8e2a2ab5d660d8a
annica-henriette <annica.helsingius@gmail.com>
2024-04-06 10:58:54 +0300

    initial commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+dirname = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+WORKOUT_FILENAME = os.getenv("WORKOUT_FILENAME") or "workout.csv"
+WORKOUT_FILE_PATH = os.path.join(dirname, "..", "data", WORKOUT_FILENAME)
+
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
+DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
