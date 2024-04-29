3be3b539e0d6a94b63abd5aa22609a432b58794f
annica-henriette <annica.helsingius@gmail.com>
2024-04-29 09:13:48 +0300

    docstring

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,4 +1,16 @@
 class User:
+    """Luokka, joka vastaa yksittäisestä käyttäjästä.
+
+    Attributes:
+        username: Merkkijono, joka kuvaa käyttäjän käyttäjätunnusta.
+        password: Merkkijono, joka kuvaa käyttäjän salasanaa.
+    """
     def __init__(self, username, password):
+        """Luokan konstruktori, joka luo uuden käyttäjän.
+
+        Args:
+            username: Merkkijono, joka kuvaa käyttäjän käyttäjätunnusta.
+            password: Merkkijono, joka kuvaa käyttäjän salasanaa.
+        """
         self.username = username
         self.password = password
